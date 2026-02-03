c6c51b4a9eff25f1cf2e5c5641be0a102d9037c7
Kairat <kairat777dk@gmail.com>
2026-02-04 01:29:25 +0500

    second

--- script.js
+++ script.js
@@ -19,3 +19,25 @@ let message = `
 `;
 
 alert(message);
+
+//Задание 2: Информация о типах данных (дополнительно)
+
+// let ten = 10;
+// let tenString = 'десять';
+// let isTen = true;
+// let nothing = null;
+// let undef;
+// let user = {
+//   name: 'Иван',
+//   age: 25,
+//   isStudent: true
+// };
+// let array = [1, 2, 3];
+
+// console.log(`Число = ${ten}, Тип данных ten = ${typeof ten}`);
+// console.log(`Строка = ${tenString}, Тип данных tenString = ${typeof tenString}`);
+// console.log(`isTen = ${isTen}, Тип данных isTen = ${typeof isTen}`);
+// console.log(`Переменная = ${nothing}, Тип данных nothing = ${typeof nothing}`);
+// console.log(`Переменная = ${undef}, Тип данных undef = ${typeof undef}`);
+// console.log(user, `Тип данных user = ${typeof user}`);
+// console.log(array, `Тип данных array = ${typeof array}`);
